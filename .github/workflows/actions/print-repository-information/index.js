4f11ad4188c499315727a0ebc7034a5397946625
kocurc <kordian.o@wp.pl>
2025-08-29 00:41:56 +0200

    Add more logging

--- .github/workflows/actions/print-repository-information/index.js
+++ .github/workflows/actions/print-repository-information/index.js
@@ -5,20 +5,18 @@ try {
     const repositoryInformationType = core.getInput("repository-information-type");
     const context = github.context;
 
+    console.log(`Chosen repository-information-type: ${repositoryInformationType}\n`);
+
     if (repositoryInformationType === "basic") {
         console.log("Print basic repository info:\n");
         console.log(`Action: ${context.action}`);
-        console.log(`Actor: ${context.actor}`);
-        console.log(`Event's name: ${context.eventName}`);
-        console.log(`API's URL: ${context.apiUrl}`);
-        console.log(`Reference: ${context.ref}`);
     }
     else if (repositoryInformationType === "extended") {
         console.log("\nPrint extended repository info:\n");
         console.log(`Event's installation ID: ${context.payload.installation.id}`);
     }
     else {
-        console.log("Insupported repository-information-type")
+        console.log("Unsupported repository-information-type")
     }
 } catch (error) {
     core.setFailed(error.message);
