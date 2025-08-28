7969e90c8cc283270f90d859781e4fe735bbfcd1
kocurc <kordian.o@wp.pl>
2025-08-29 00:38:10 +0200

    Add index logic

--- .github/workflows/actions/print-repository-information/index.js
+++ .github/workflows/actions/print-repository-information/index.js
@@ -1,14 +1,25 @@
 const core = require('@actions/core');
-const github = requure('@actions/github');
+const github = require('@actions/github');
 
 try {
-    const name = core.getInput('name', {
-        required: true
-    });
-    const greeting = `Hello, ${name}`;
+    const repositoryInformationType = core.getInput("repository-information-type");
+    const context = github.context;
 
-    console.log(greeting);
-    core.setOutput('greeting', greeting);
+    if (repositoryInformationType === "basic") {
+        console.log("Print basic repository info:\n");
+        console.log(`Action: ${context.action}`);
+        console.log(`Actor: ${context.actor}`);
+        console.log(`Event's name: ${context.eventName}`);
+        console.log(`API's URL: ${context.apiUrl}`);
+        console.log(`Reference: ${context.ref}`);
+    }
+    else if (repositoryInformationType === "extended") {
+        console.log("\nPrint extended repository info:\n");
+        console.log(`Event's installation ID: ${context.payload.installation.id}`);
+    }
+    else {
+        console.log("Insupported repository-information-type")
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
