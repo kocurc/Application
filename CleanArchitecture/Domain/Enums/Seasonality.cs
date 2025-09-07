a11de6c59a5e2e5c0de7e558b8d2c166f010da7d
kocurc <kordian.o@wp.pl>
2025-09-07 22:20:20 +0200

    Check documentation changed condition

--- CleanArchitecture/Domain/Enums/Seasonality.cs
+++ CleanArchitecture/Domain/Enums/Seasonality.cs
@@ -6,12 +6,12 @@
 	public enum Seasonality
 	{
 		/// <summary>
-		/// One period per year.
+		/// One period per year
 		/// </summary>
 		Yearly = 1,
 
 		/// <summary>
-		/// Twelve periods per year.
+		/// Twelve periods per year
 		/// </summary>
 		Monthly = 12,
 	}
